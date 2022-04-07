8da9b36a5610a52de7d93f6f669fb3be3857887f
Igor Prokazov <smitt8@yandex.ru>
2022-04-07 21:55:31 +0300

    fix: Исправить ошибку с размером картинки в карточке

--- script/index.js
+++ script/index.js
@@ -1,9 +1,56 @@
-let profile = document.querySelector('.content').querySelector('.profile');
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const content = document.querySelector('.content');
+const templateCard = document.querySelector('#template-card');
+
+function createCard(name, link) {
+  const card = templateCard.content.firstElementChild.cloneNode(true);
+  const photo = card.querySelector('.card__photo');
+  photo.src = link;
+  photo.alt = name;
+
+  card.querySelector('.card__heading').textContent = name;
+
+  return card;
+}
+
+const cards = content.querySelector('.photo-grid');
+
+initialCards.forEach(element => {
+  cards.append(createCard(element.name, element.link));
+});
+
+const profile = content.querySelector('.profile');
 profile.querySelector('.profile__edit-btn').addEventListener('click', editHandler);
 
-let popUp = document.querySelector('.popup');
-let popUpContainer = popUp.querySelector('.popup__container');
-let popUpForm = popUpContainer.querySelector('.popup__form');
+const popUp = document.querySelector('.popup');
+const popUpContainer = popUp.querySelector('.popup__container');
+const popUpForm = popUpContainer.querySelector('.popup__form');
 
 popUpContainer.querySelector('.popup__close-btn').addEventListener('click', editHandler);
 popUpForm.addEventListener('submit', saveHandler);

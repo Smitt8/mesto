d98b34a14df28a01b1edbacaae654fd80c99a8a8
Igor Prokazov <smitt8@yandex.ru>
2022-05-28 01:01:15 +0300

    refeac: Вынести поп-ап в отдельный класс

---
+++ scripts/Popup.js
@@ -0,0 +1,31 @@
+export default class Popup {
+  constructor(popupSelector) {
+    this._popup = document.querySelector(popupSelector)
+  }
+
+  _handleEscClick = (event) => {
+    if (event.key === 'Escape') {
+      this.close();
+    }
+  };
+
+  _handleClosePopup = (event) => {
+    if (event.target === event.currentTarget || event.target.classList.contains('popup__close-btn')) {
+      this.close();
+    }
+  }
+
+  close() {
+    this._popup.classList.remove('popup_opened');
+    document.removeEventListener('keydown', this._handleEscClick);
+  }
+
+  open = () => {
+      this._popup.classList.add('popup_opened');
+      document.addEventListener('keydown', this._handleEscClick);
+  }
+
+  setEventsListeners() {
+    this._popup.addEventListener('mousedown', this._handleClosePopup);
+  }
+}
